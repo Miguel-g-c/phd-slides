921154d6d9065061787a7f12ca9b8a13d02d4e55
Miguel G. Casas <miguel.garcia-casas@normative.io>
2024-01-28 20:39:01 +0100

    Define MOOPs content collection

---
+++ src/content/config.ts
@@ -0,0 +1,25 @@
+import { z, defineCollection } from "astro:content";
+
+const decisionVariablesSchema = z.object({
+  names: z.array(z.string()),
+  values: z.array(z.array(z.union([z.number(), z.string()]))),
+});
+
+const objectivesSchema = z.object({
+  names: z.array(z.string()),
+  values: z.array(z.array(z.number())),
+});
+
+const moopSchema = z.object({
+  decisionVariables: decisionVariablesSchema,
+  objectives: objectivesSchema,
+});
+
+const moopCollection = defineCollection({
+  type: "data",
+  schema: moopSchema,
+});
+
+export const collections = {
+  moops: moopCollection,
+};
